7a68f29c84b70aaf827b587e7da3220b42e5f47a
Abdul Rehman <malikrehmanshabir786@gmail.com>
2023-11-25 12:04:51 +0500

    Assignment 4

---
+++ if else.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+int main(){
+int c;
+cin>>c;
+if(c>18)
+
+cout<< "you are already mature"<<endl;
+
+else if(c==18)
+
+cout<< "You are mature now"<<endl;
+
+else
+cout<< "You are not mature Yet"<<endl;
+
+    return 0;
+}
